48aed6343a8fd3952b31abc616275e8054595097
Yassine Idrissi <yaidriss@c2r1p7.1337.ma>
2022-05-24 23:43:21 +0100

    with all test

--- libft.h
+++ libft.h
@@ -1,5 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
+/* ************************************************************************** */ /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
@@ -24,5 +23,8 @@ int 			ft_str_is_uppercase(char *str);
 int 			ft_str_is_printable(char *str);
 char 			*ft_strupcase(char *str);
 char 			*ft_strlowcase(char *str);
+unsigned int 	ft_strlcpy(char *dest, char *src, unsigned int size);
+void 			ft_putstr_non_printable(char *str);
+char 			*ft_strcapitalize(char *str);
 
 #endif
